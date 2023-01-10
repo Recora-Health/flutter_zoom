7a93952ecac6759088766993baa379c57233e759
damonique-rh <damonique@recorahealth.com>
2023-01-10 14:50:32 -0500

    whenJoinMeeting:

--- zoom/ios/Classes/SwiftZoomPlugin.swift
+++ zoom/ios/Classes/SwiftZoomPlugin.swift
@@ -116,7 +116,7 @@ public class SwiftZoomPlugin: NSObject, FlutterPlugin,FlutterStreamHandler , Mob
             
             meetingSettings?.disableDriveMode(parseBoolean(data: arguments["disableDrive"]!, defaultValue: false))
             meetingSettings?.disableCall(in: parseBoolean(data: arguments["disableDialIn"]!, defaultValue: false))
-            meetingSettings?.setAutoConnectInternetAudio(true)
+            meetingSettings?.setAutoConnectInternetAudio(whenJoinMeeting: true)
             meetingSettings?.disableShowVideoPreview(true)
             meetingSettings?.setMuteAudioWhenJoinMeeting(parseBoolean(data: arguments["noAudio"]!, defaultValue: false))
             meetingSettings?.meetingShareHidden = parseBoolean(data: arguments["disableShare"]!, defaultValue: false)
