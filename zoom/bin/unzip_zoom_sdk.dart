16ac9b75a3d9219f300c67c43fb4fd1099cdcc84
damonique-rh <damonique@recorahealth.com>
2023-06-12 11:51:13 -0400

    Add command argument to update local SDK version

--- zoom/bin/unzip_zoom_sdk.dart
+++ zoom/bin/unzip_zoom_sdk.dart
@@ -1,8 +1,13 @@
 import 'dart:core';
 import 'dart:io';
 import 'dart:convert';
+import 'package:args/args.dart';
 
 void main(List<String> args) async {
+  final parser = ArgParser()..addFlag('update', negatable: false, abbr: 'u');
+  final results = parser.parse(args);
+  final isUpdate = results['update'] as bool;
+
   var location = Platform.script.toString();
   var isNewFlutter = location.contains(".snapshot");
   if (isNewFlutter) {
@@ -34,42 +39,43 @@ void main(List<String> args) async {
   }
   if (!isNewFlutter) location = location.replaceFirst("/bin/unzip_zoom_sdk.dart", "");
 
-  await checkAndDownloadSDK(location);
+  await checkAndDownloadSDK(location, isUpdate);
   print('Complete');
 }
 
-Future<void> checkAndDownloadSDK(String location) async {
+Future<void> checkAndDownloadSDK(String location, bool isUpdate) async {
   var androidCommonLibFile = location + '/android/libs/commonlib.aar';
   var exists = await File(androidCommonLibFile).exists();
-  if (!exists) {
-    await downloadFile(Uri.parse('https://www.dropbox.com/s/wimmggu7tjnbr4w/commonlib.aar?dl=1'), androidCommonLibFile);
+  if (!exists || isUpdate) {
+    await downloadFile(
+        Uri.parse('https://www.dropbox.com/s/wimmggu7tjnbr4w/commonlib.aar?dl=1'), androidCommonLibFile, isUpdate);
   }
   var androidRTCLibFile = location + '/android/libs/mobilertc.aar';
   exists = await File(androidRTCLibFile).exists();
-  if (!exists) {
-    await downloadFile(Uri.parse('https://www.dropbox.com/s/p6q40yw4akvyimx/mobilertc.aar?dl=1'), androidRTCLibFile);
+  if (!exists || isUpdate) {
+    await downloadFile(
+        Uri.parse('https://www.dropbox.com/s/p6q40yw4akvyimx/mobilertc.aar?dl=1'), androidRTCLibFile, isUpdate);
   }
 
   var iosSDKFile = location + '/ios/MobileRTC.xcframework/ios-arm64_armv7/MobileRTC.framework/MobileRTC';
   exists = await File(iosSDKFile).exists();
 
-  if (!exists) {
-    await downloadFile(Uri.parse('https://www.dropbox.com/s/zdzfl0ick5wdogo/arm64-MobileRTC?dl=1'), iosSDKFile);
+  if (!exists || isUpdate) {
+    await downloadFile(
+        Uri.parse('https://www.dropbox.com/s/zdzfl0ick5wdogo/arm64-MobileRTC?dl=1'), iosSDKFile, isUpdate);
   }
 
   var iosSimulateSDKFile =
       location + '/ios/MobileRTC.xcframework/ios-i386_x86_64-simulator/MobileRTC.framework/MobileRTC';
   exists = await File(iosSimulateSDKFile).exists();
-  if (!exists) {
-    await downloadFile(
-      Uri.parse('https://www.dropbox.com/s/2t3u21bn9zatyb6/x86_64-simulator-MobileRTC?dl=1'),
-      iosSimulateSDKFile,
-    );
+  if (!exists || isUpdate) {
+    await downloadFile(Uri.parse('https://www.dropbox.com/s/2t3u21bn9zatyb6/x86_64-simulator-MobileRTC?dl=1'),
+        iosSimulateSDKFile, isUpdate);
   }
 }
 
-Future<void> downloadFile(Uri uri, String savePath) async {
-  print('Download ${uri.toString()} to $savePath');
+Future<void> downloadFile(Uri uri, String savePath, bool isUpdate) async {
+  print('${isUpdate ? 'Updating' : 'Downloading'} ${uri.toString()} to $savePath');
   File destinationFile = await File(savePath).create(recursive: true);
   final request = await HttpClient().getUrl(uri);
   final response = await request.close();
--- zoom/zoom_platform_interface/lib/zoom_platform_interface.dart
+++ zoom/zoom_platform_interface/lib/zoom_platform_interface.dart
@@ -13,7 +13,7 @@ export 'zoom_options.dart';
 abstract class ZoomPlatform extends PlatformInterface {
   ZoomPlatform() : super(token: _token);
   static final Object _token = Object();
-  static ZoomPlatform _instance = MethodChannelZoom() as ZoomPlatform;
+  static ZoomPlatform _instance = MethodChannelZoom();
   static ZoomPlatform get instance => _instance;
   static set instance(ZoomPlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
