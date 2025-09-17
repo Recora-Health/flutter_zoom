b1dd2a0f882b9beed70e734ffe86c0d003139419
John Hamson <jhamson@techiemon.com>
2025-09-17 10:05:31 -0500

    trying ios 6.4.5

--- zoom/bin/unzip_zoom_sdk.dart
+++ zoom/bin/unzip_zoom_sdk.dart
@@ -73,7 +73,7 @@ Future<void> checkAndDownloadSDK(String location, bool isUpdate) async {
   // If the files do not exist or we are updating, then download the latest version
   if (!exists || isUpdate) {
     await downloadFile(
-        Uri.parse('https://www.dropbox.com/scl/fi/d1ntegl4h7g6juhtf19wd/arm64-MobileRTC?rlkey=18h5dzehdg619vf9la3prtqed&st=unxx1ave&dl=1'), iosSDKFile, isUpdate);
+        Uri.parse('https://www.dropbox.com/scl/fi/1l4dt39tl7n53avlz4a93/x86_64-simulator-MobileRTC?rlkey=bo72g347cin4tuutnoqo1zzif&st=ro966gxy&dl=1'), iosSDKFile, isUpdate);
   }
 
   // Check if the Arm64 iOS Embulator SDK files exist
@@ -86,7 +86,7 @@ Future<void> checkAndDownloadSDK(String location, bool isUpdate) async {
   if (!exists || isUpdate) {
     await downloadFile(
         Uri.parse(
-            'https://www.dropbox.com/scl/fi/fdkfqi34jfag0xwv6sw8h/x86_64-simulator-MobileRTC?rlkey=psx3rkd593hjpiqrevcddqhgu&st=dvg5eqku&dl=1'),
+            'https://www.dropbox.com/scl/fi/1l4dt39tl7n53avlz4a93/x86_64-simulator-MobileRTC?rlkey=bo72g347cin4tuutnoqo1zzif&st=ro966gxy&dl=1'),
         iosSimulateArm64SDKFile,
         isUpdate);
   }
