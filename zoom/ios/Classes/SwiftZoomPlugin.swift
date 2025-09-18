232d84a60f7433d346d9ea065fa848dbe7c1c60f
John Hamson <jhamson@techiemon.com>
2025-09-18 10:39:46 -0500

    seeming progress, fix duplicate

--- zoom/ios/Classes/SwiftZoomPlugin.swift
+++ zoom/ios/Classes/SwiftZoomPlugin.swift
@@ -353,7 +353,7 @@ import MobileRTC
 }
 
  
-@objc(SwiftZoomPlugin) public class AuthenticationDelegate: NSObject, MobileRTCAuthDelegate {
+@objc(AuthenticationDelegate) public class AuthenticationDelegate: NSObject, MobileRTCAuthDelegate {
     
     private var result: FlutterResult?
     
