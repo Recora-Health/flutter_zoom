c5bbcd59c70416f4f5c8c2a5d9bf78668868611c
John Hamson <jhamson@techiemon.com>
2025-12-15 19:14:45 -0600

    Disable preview screens

--- zoom/android/src/main/java/com/webcare/zoom/ZoomPlugin.java
+++ zoom/android/src/main/java/com/webcare/zoom/ZoomPlugin.java
@@ -182,7 +182,7 @@ public class ZoomPlugin implements FlutterPlugin, MethodCallHandler,ActivityAwar
         // Configure settings before joining
         meetingSettingsHelper.enable720p(false);
         meetingSettingsHelper.enableShowMyMeetingElapseTime(true);
-        meetingSettingsHelper.setShowVideoPreviewWhenJoinMeeting(false);
+        meetingSettingsHelper.disableShowVideoPreviewWhenJoinMeeting(true);
 
         // Join the meeting with the configured options
         meetingService.joinMeetingWithParams(context, params, opts);
